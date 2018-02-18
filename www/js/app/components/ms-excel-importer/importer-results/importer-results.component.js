d631c16e35811c13fdcbb02d6e81e1dfeea91611
Javier Colmenárez <jcolmenarz@gmail.com>
2018-02-17 22:34:52 -0400

    Improving the Importer Results view's UI: Adding a quick info section to show the database settings to use in the server queries, Creating a restart logic and button, where the whole process can be started again from the very beginning, Creating a retry logic and button, where the failed and unprocessed registers can be resubmitted again with just one click, Updating FontAwesome to version 5.0.6 and applying the required changes and improvements from the update, Removing the old unused FontAwesome web fonts files.

--- www/js/app/components/ms-excel-importer/importer-results/importer-results.component.js
+++ www/js/app/components/ms-excel-importer/importer-results/importer-results.component.js
@@ -4,11 +4,13 @@ export const MSExcelImporterResultsComponent = {
     };
 
 
-MSExcelImporterResultsController.$inject = ['$filter', '$stateParams', 'MSExcelImporterServices'];
+MSExcelImporterResultsController.$inject = ['$filter', '$state', '$stateParams', 'MSExcelImporterServices'];
 
-function MSExcelImporterResultsController ($filter, $stateParams, MSExcelImporterServices){
+function MSExcelImporterResultsController ($filter, $state, $stateParams, MSExcelImporterServices){
 
     const ctrl = this;
+
+    let failedPositionsArray = [];
 
     this.$onInit = () => {
 
@@ -18,8 +20,9 @@ function MSExcelImporterResultsController ($filter, $stateParams, MSExcelImporte
         ctrl.isEventPromoCodeMode = $stateParams.isEventPromoCodeMode;
         ctrl.showingSummaryTable = false;
         ctrl.summaryTableLabel = 'General';
-        ctrl.summaryTableData = null;
+        ctrl.summaryTableData = [];
         ctrl.isProcessingCompleted = false;
+        ctrl.submissionCancelled = false;
         ctrl.showingResults = false;
         ctrl.results = {};
         ctrl.successfulRegisters = [];
@@ -80,6 +83,7 @@ function MSExcelImporterResultsController ($filter, $stateParams, MSExcelImporte
             ctrl.results[row.StudentId] = {
                 'isComplete': false,
                 'isError': false,
+                'isQuerying': 'Not yet',
                 'messages': [
                     `${initialMssg} for ${row.StudentId}:`,
                     `${dateAndTimeInfo()}: The request has been created.`
@@ -113,7 +117,72 @@ function MSExcelImporterResultsController ($filter, $stateParams, MSExcelImporte
 
         ctrl.showingSummaryTable = type !== 'General';
         ctrl.summaryTableLabel = type;
-        ctrl.summaryTableData = type !== 'General' ? (type === 'Failed' ? ctrl.failedRegisters : ctrl.successfulRegisters) : null;
+        ctrl.summaryTableData = type !== 'General' ? (type === 'Failed' ? ctrl.failedRegisters : ctrl.successfulRegisters) : [];
+
+    };
+
+    this.startAgain = () => {
+
+        failedPositionsArray = [];
+        failedPositionsArray.length = 0;
+        ctrl.summaryTableData = [];
+        ctrl.summaryTableData.length = 0;
+        ctrl.isProcessingCompleted = false;
+        ctrl.submissionCancelled = false;
+        ctrl.showingResults = false;
+        ctrl.results = {};
+        ctrl.successfulRegisters = [];
+        ctrl.successfulRegisters.length = 0;
+        ctrl.failedRegisters = [];
+        ctrl.failedRegisters.length = 0;
+        ctrl.registersInProcess = 0;
+        ctrl.dbConfigHasChanged = false;
+        ctrl.dbNewConfig = {
+            server: null,
+            port: null,
+            database: null,
+            user: null,
+            password: null
+        };
+
+        $state.go('msExcelImporter');
+
+    };
+
+    this.cancelSubmission = () => {
+
+        ctrl.submissionCancelled = true;
+        ctrl.isProcessingCompleted = true;
+
+    };
+
+    this.retryFailedAndUnprocessed = () => {
+
+        ctrl.submissionCancelled = false;
+        ctrl.isProcessingCompleted = false;
+
+        if (ctrl.failedRegisters.length > 0){
+
+            ctrl.registersInProcess += ctrl.failedRegisters.length;
+
+            angular.forEach(ctrl.failedRegisters, fail => {
+
+                ctrl.results[fail.StudentId].isComplete = false;
+                ctrl.results[fail.StudentId].isError = false;
+                ctrl.results[fail.StudentId].isQuerying = 'Not yet';
+                failedPositionsArray.push(fail.query - 1);
+
+            });
+
+            ctrl.failedRegisters = [];
+            ctrl.failedRegisters.length = 0;
+            processRow(failedPositionsArray[0], true);
+
+        } else if (ctrl.registersInProcess > 0){
+
+            processRow(ctrl.excelData.length - ctrl.registersInProcess);
+
+        }
 
     };
 
@@ -124,13 +193,48 @@ function MSExcelImporterResultsController ($filter, $stateParams, MSExcelImporte
 
     };
 
-    const processRow = n => {
+    const decidingToProcessAnotherRow = (n, fromList) => {
+
+        if ( angular.isDefined(fromList) ){
+
+            failedPositionsArray.shift();
+            
+            if ( failedPositionsArray.length >= 1 ){
+
+                processRow(failedPositionsArray[0], true);
+
+            } else if ( ctrl.registersInProcess == 0 ){
+
+                failedPositionsArray = [];
+                failedPositionsArray.length = 0;
+                ctrl.isProcessingCompleted = true;
+
+            } else {
+
+                processRow(ctrl.excelData.length - ctrl.registersInProcess);
+
+            }
+
+        } else if ( !ctrl.isProcessingCompleted && ctrl.excelData.length > (n + 1) ){
+
+            processRow(n + 1);
+
+        } else {
+
+            ctrl.isProcessingCompleted = true;
+
+        }
+
+    };
+
+    const processRow = (n, fromList) => {
 
         // Preparing the POST data
         let row = ctrl.excelData[n],
             postData = {'studentId': row.StudentId},
             initialMssg = ctrl.isEventPromoCodeMode ? 'The event promotion code insertion' : 'The event enrollment';
 
+        ctrl.results[row.StudentId].isQuerying = 'Yes';
         ctrl.results[row.StudentId].messages.push(`
             ${dateAndTimeInfo()}: The database query has been started.
         `);
@@ -165,19 +269,12 @@ function MSExcelImporterResultsController ($filter, $stateParams, MSExcelImporte
 
             ctrl.results[row.StudentId].isComplete = true;
             ctrl.results[row.StudentId].isError = false;
+            ctrl.results[row.StudentId].isQuerying = 'Not anymore';
             ctrl.results[row.StudentId].messages.push(`
                 ${dateAndTimeInfo()}: The request has been successfully completed.
             `);
 
-            if ( ctrl.excelData.length > (n + 1) ){
-
-                processRow(n + 1);
-
-            } else {
-
-                ctrl.isProcessingCompleted = true;
-
-            }
+            decidingToProcessAnotherRow(n, fromList);
 
         }).catch(error => {
             
@@ -197,20 +294,13 @@ function MSExcelImporterResultsController ($filter, $stateParams, MSExcelImporte
 
             ctrl.results[row.StudentId].isComplete = true;
             ctrl.results[row.StudentId].isError = true;
+            ctrl.results[row.StudentId].isQuerying = 'Not anymore';
             ctrl.results[row.StudentId].messages.push(`
                 ${dateAndTimeInfo()}: The request has failed due to the following reason: 
                 ${err.code} - ${err.name}: ${errorMssg}
             `);
 
-            if ( ctrl.excelData.length > (n + 1) ){
-
-                processRow(n + 1);
-
-            } else {
-
-                ctrl.isProcessingCompleted = true;
-
-            }
+            decidingToProcessAnotherRow(n, fromList);
 
         });
 
